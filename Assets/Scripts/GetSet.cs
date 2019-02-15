27e47724b6f01ce23f16ef09f384e287e9afb2f2
darshee-m <darshee.m@somaiya.edu>
2019-02-15 17:30:05 +0530

    final

---
+++ Assets/Scripts/GetSet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetSet : MonoBehaviour
+{
+
+    public InputField text1;
+    public Text name1;
+    public bool createLabel = false;
+
+    public void setget()
+    {
+        createLabel = true;
+        name1.text = " " + text1.text;
+        createLabel = false;
+    }
+
+}
